10c62ea4313106b61d1698c98166c21adf629958
Baroshem <jakub.andrzejewski.dev@gmail.com>
2023-01-29 23:01:33 +0100

    fix: nuxt 3.1.X x-powered-by

--- src/runtime/nitro.ts
+++ src/runtime/nitro.ts
@@ -2,6 +2,11 @@ import type { NitroAppPlugin, RenderResponse } from 'nitropack'
 
 export default <NitroAppPlugin> function (nitro) {
   nitro.hooks.hook('render:response', (response: RenderResponse) => {
-    delete response.headers['X-Powered-By']
+    // Temporary as in Nuxt 3.0.0 header name is 'X-Powered-By' and in 3.1.X is 'x-powered-by'
+    if (response.headers['x-powered-by']) {
+      delete response.headers['x-powered-by']
+    } else if (response.headers['X-Powered-By']) {
+      delete response.headers['X-Powered-By']
+    }
   })
 }
