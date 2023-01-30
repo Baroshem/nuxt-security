981b76c94f6518f6b6221e1b50f58aa86598b72e
Jakub Andrzejewski <37120330+Baroshem@users.noreply.github.com>
2023-01-30 10:38:11 +0100

    fix: stackblitz demo

--- .stackblitz/nuxt.config.ts
+++ .stackblitz/nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['nuxt-security']
-})
+  modules: ['nuxt-security'],
+  // Following configuration is only necessary to make Stackblitz work correctly. 
+  // For local projects, you do not need any configuration to try it out.
+  security: {
+    headers: {
+      crossOriginResourcePolicy: {
+        value: 'cross-origin',
+        route: '/**',
+      },
+      contentSecurityPolicy: false,
+      xFrameOptions: false,
+    },
+  },
+});
