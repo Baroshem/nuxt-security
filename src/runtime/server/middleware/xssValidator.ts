422732ceaa16ed4dbfd8603e008a1478141106b9
Baroshem <jakub.andrzejewski.dev@gmail.com>
2023-07-27 17:35:03 +0200

    fix #185: errors in xss validation

--- src/runtime/server/middleware/xssValidator.ts
+++ src/runtime/server/middleware/xssValidator.ts
@@ -4,24 +4,38 @@ import { getRouteRules } from "#imports";
 
 export default defineEventHandler(async (event) => {
   const routeRules = getRouteRules(event);
+
   const xssValidator = new FilterXSS(routeRules.security.xssValidator);
 
-  if (routeRules.security.xssValidator !== false) {
-    if (["POST", "GET"].includes(event.node.req.method!!)) {
-      const valueToFilter =
-        event.node.req.method === "GET" ? getQuery(event) : await readBody(event);
+  if (event.node.req.socket.readyState !== "readOnly") {
+    if (routeRules.security.xssValidator !== false) {
+      if (["POST", "GET"].includes(event.node.req.method!!)) {
+        const valueToFilter =
+          event.node.req.method === "GET"
+            ? getQuery(event)
+            : await readBody(event);
         // Fix for problems when one middleware is returning an error and it is catched in the next
-      if (valueToFilter && Object.keys(valueToFilter).length) {
-        if (valueToFilter.statusMessage && valueToFilter.statusMessage !== 'Bad Request') return
-        const stringifiedValue = JSON.stringify(valueToFilter);
-        const processedValue = xssValidator.process(JSON.stringify(valueToFilter));
-        if (processedValue !== stringifiedValue) {
-          const badRequestError = { statusCode: 400, statusMessage: "Bad Request" };
-          if (routeRules.security.requestSizeLimiter.throwError === false) {
-            return badRequestError;
-          }
+        if (valueToFilter && Object.keys(valueToFilter).length) {
+          if (
+            valueToFilter.statusMessage &&
+            valueToFilter.statusMessage !== "Bad Request"
+          )
+            return;
+          const stringifiedValue = JSON.stringify(valueToFilter);
+          const processedValue = xssValidator.process(
+            JSON.stringify(valueToFilter)
+          );
+          if (processedValue !== stringifiedValue) {
+            const badRequestError = {
+              statusCode: 400,
+              statusMessage: "Bad Request",
+            };
+            if (routeRules.security.requestSizeLimiter.throwError === false) {
+              return badRequestError;
+            }
 
-          throw createError(badRequestError);
+            throw createError(badRequestError);
+          }
         }
       }
     }
