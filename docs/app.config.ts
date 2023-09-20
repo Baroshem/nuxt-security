e1ac64f1cd8e266b78b7ed71d999141addb3e0dd
Baroshem <jakub.andrzejewski.dev@gmail.com>
2023-09-20 13:55:36 +0200

    docs: rewrite

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,8 +1,14 @@
 export default defineAppConfig({
+  github: {
+    owner: 'Baroshem',
+    repo: 'nuxt-security',
+    branch: 'main'
+  },
   docus: {
     title: 'Nuxt Security',
     description: '🛡️ Security Module for Nuxt based on HTTP Headers and Middleware',
     image: '/preview.jpg',
+    url: 'https://nuxt-security.vercel.app',
     socials: {
       twitter: 'jacobandrewsky',
       github: 'baroshem/nuxt-security',
@@ -15,15 +21,19 @@ export default defineAppConfig({
     aside: {
       level: 1
     },
-    // github: {
-    //   dir: 'docs/content',
-    //   root: 'docs/content',
-    //   edit: true,
-    //   releases: true,
-    //   owner: 'baroshem',
-    //   repo: 'nuxt-security',
-    //   branch: 'main'
-    // },
+    github: {
+      dir: 'docs/content',
+      root: 'docs/content',
+      edit: true,
+      releases: true,
+      owner: 'baroshem',
+      repo: 'nuxt-security',
+      branch: 'main'
+    },
+    cover: {
+      src: '/preview.png',
+      alt: 'Security Module for Nuxt based on HTTP Headers and Middleware'
+    },
     header: {
       logo: true
     }
