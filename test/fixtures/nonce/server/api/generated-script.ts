a19b3f3c49609c44df192e1d646142243d8e14cb
Sébastien Raffray <sraffray@yahoo.fr>
2023-10-18 00:11:42 +0200

    use autoImports in playground and fixtures

--- test/fixtures/nonce/server/api/generated-script.ts
+++ test/fixtures/nonce/server/api/generated-script.ts
@@ -1,5 +1,3 @@
-import { defineEventHandler } from 'h3'
-
 export default defineEventHandler((event) => {
   event.node.res.setHeader('Content-Type', 'application/javascript')
   return 'console.log("Hello World")'
