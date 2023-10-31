443d426d59fb72945ebc44e09da5ccdd054e4301
Sébastien Raffray <sraffray@yahoo.fr>
2023-10-31 19:18:53 +0100

    use cheerio to parse HTML

--- src/runtime/nitro/plugins/99-cspNonce.ts
+++ src/runtime/nitro/plugins/99-cspNonce.ts
@@ -1,21 +1,12 @@
 import { defineNitroPlugin } from '#imports'
 import type { H3Event } from 'h3'
+import * as cheerio from 'cheerio'
 
-// To prevent the nonce attribute from being added to literal strings,
-// we need to make sure that the tag is not preceded by a single or double quote.
-// This is done by using a negative lookbehind assertion. See https://www.regular-expressions.info/lookaround.html
-// See https://regex101.com/r/DBE57j/1 for some examples.
-const tagNotPrecededByQuotes = (tag: string) => new RegExp(`(?<!['|"])<${tag}`, 'g')
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:html', (html, { event }) => {
     if (isPrerendering(event)) {
       // In SSG mode, do not inject nonces in html
-      // However first make sure we erase nonce placeholders from CSP meta
-      html.head = html.head.map((meta) => {
-        if (!meta.startsWith('<meta http-equiv="Content-Security-Policy"')) { return meta }
-        return meta.replaceAll("'nonce-{{nonce}}'", '')
-      })
       return
     }
     const nonce = parseNonce(`${event.node.res.getHeader('Content-Security-Policy')}`)
@@ -28,17 +19,21 @@ export default defineNitroPlugin((nitroApp) => {
       return meta.replaceAll('{{nonce}}', nonce)
     })
 
-    // Add nonce attribute to all link tags
-    html.head = html.head.map(link => link.replaceAll(tagNotPrecededByQuotes('link'), `<link nonce="${nonce}"`))
-    html.bodyAppend = html.bodyAppend.map(link => link.replaceAll(tagNotPrecededByQuotes('link'), `<link nonce="${nonce}"`))
+    // Scan all relevant sections of the NuxtRenderHtmlContext
+    for (const section of ['body', 'bodyAppend', 'bodyPrepend', 'head']) {
+      const htmlRecords = html as unknown as Record<string, string[]>
 
-    // Add nonce attribute to all script tags
-    html.head = html.head.map(script => script.replaceAll(tagNotPrecededByQuotes('script'), `<script nonce="${nonce}"`))
-    html.bodyAppend = html.bodyAppend.map(script => script.replaceAll(tagNotPrecededByQuotes('script'), `<script nonce="${nonce}"`))
-
-    // Add nonce attribute to all style tags
-    html.head = html.head.map(style => style.replaceAll(tagNotPrecededByQuotes('style'), `<style nonce="${nonce}"`))
-    html.bodyAppend = html.bodyAppend.map(style => style.replaceAll(tagNotPrecededByQuotes('style'), `<style nonce="${nonce}"`))
+      htmlRecords[section] = htmlRecords[section].map(element => {
+        const $ = cheerio.load(element, null, false)
+        // Add nonce to all link tags
+        $('link').attr('nonce', nonce)
+        // Add nonce to all script tags
+        $('script').attr('nonce', nonce)
+        // Add nonce to all style tags
+        $('style').attr('nonce', nonce)
+        return $.html()
+      })
+    }
   })
 
   function parseNonce (content: string) {
