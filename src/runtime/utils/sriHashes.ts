25c9bbb15ec56d7a68a083d8916ba717f11a0119
Sébastien Raffray <sraffray@yahoo.fr>
2023-11-08 08:13:18 +0100

    fix ci tests failing

--- src/runtime/utils/sriHashes.ts
+++ src/runtime/utils/sriHashes.ts
@@ -11,17 +11,23 @@ export default async function (nitro: Nitro) {
   // Will be later necessary to construct url
   const { cdnURL: appCdnUrl = '', baseURL: appBaseUrl } = nitro.options.runtimeConfig.app
 
+
   // Go through all public assets folder by folder
   const publicAssets = nitro.options.publicAssets
   for (const publicAsset of publicAssets) {
     const { dir, baseURL = '' } = publicAsset
-    // TBD
-    // recursive option on readdir is supported since NodeJS 18
-    // do we have a minimum build engine requirement ?
-    const entries = await readdir(dir, { withFileTypes: true, recursive: true })
+
+    // Node 16 compatibility maintained
+    // Node 18.17+ supports recursive option on readdir
+    // const entries = await readdir(dir, { withFileTypes: true, recursive: true })
+    const entries = await readdir(dir, { withFileTypes: true })
     for (const entry of entries) {
       if (entry.isFile()) {
-        const fullPath = join(entry.path, entry.name)
+
+        // Node 16 compatibility maintained
+        // Node 18.17+ supports entry.path on DirEnt
+        // const fullPath = join(entry.path, entry.name)
+        const fullPath = join(dir, entry.name)
         const fileContent = await readFile(fullPath)
         const hash = generateHash(fileContent, hashAlgorithm)
         // construct the url as it will appear in the head template
