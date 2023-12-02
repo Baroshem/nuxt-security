23274069ab73ec31e27d726bf5d62dd94522e865
becem-gharbi <becem.gharbi@live.com>
2023-12-02 20:33:41 +0100

    refactor(routeRules): replace undefined middlewares with false
    Previously, a disabled middleware is not defined on route rules. Now, it is defined and set to false.

--- src/runtime/nitro/plugins/00-routeRules.ts
+++ src/runtime/nitro/plugins/00-routeRules.ts
@@ -17,18 +17,16 @@ export default defineNitroPlugin((nitroApp) => {
     const { headers, enabled, hidePoweredBy, ...rest } = securityOptions
     for (const middleware in rest) {
       const middlewareConfig = securityOptions[middleware as keyof typeof securityOptions] as any
-      if (typeof middlewareConfig !== 'boolean') {
-        const middlewareRoute = '/**'
+      const middlewareRoute = '/**'
       nitroRouteRules![middlewareRoute] = {
         ...nitroRouteRules![middlewareRoute],
         security: {
           ...nitroRouteRules![middlewareRoute]?.security,
-          [SECURITY_MIDDLEWARE_NAMES[middleware]]: {
+          [SECURITY_MIDDLEWARE_NAMES[middleware]]: middlewareConfig && {
             ...middlewareConfig,
             throwError: middlewareConfig.throwError
           }
         }
-      }
       }
     }
   }
