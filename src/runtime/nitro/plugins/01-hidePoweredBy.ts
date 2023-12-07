d8d1904f708f9e0d54969fe65a87bcdc0118b0fe
Sébastien Raffray <sraffray@yahoo.fr>
2023-12-07 16:02:01 +0100

    Do not remove headers if response already sent

--- src/runtime/nitro/plugins/01-hidePoweredBy.ts
+++ src/runtime/nitro/plugins/01-hidePoweredBy.ts
@@ -2,6 +2,8 @@ import { defineNitroPlugin, removeResponseHeader } from '#imports'
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('beforeResponse', (event) => {
-    removeResponseHeader(event, 'x-powered-by')
+    if (!event.node.res.headersSent) {
+      removeResponseHeader(event, 'x-powered-by')
+    }
   })
 })
