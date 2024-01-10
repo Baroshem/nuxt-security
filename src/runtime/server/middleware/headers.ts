c58491a204a60d920df7b419d855d6b7ad7b64ca
julien huang <julien.huang@outlook.fr>
2024-01-10 20:10:52 +0100

    chore: lint

--- src/runtime/server/middleware/headers.ts
+++ src/runtime/server/middleware/headers.ts
@@ -3,10 +3,10 @@ import { defineEventHandler, setHeader, removeResponseHeader } from '#imports'
 export default defineEventHandler((event) => {
     if(event.context.security.headers) {  
         Object.entries(event.context.security.headers).forEach(([header, value]) => {
-            if(value === false) {
+            if (value === false) {
                 removeResponseHeader(event, header)
-            }else {
-                setHeader(event, header, value, )
+            } else {
+                setHeader(event, header, value)
             }
         }) 
     }
