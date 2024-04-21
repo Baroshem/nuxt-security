f141124dccba2afd31ec622011315e757521d9d3
Sébastien Raffray <sraffray@yahoo.fr>
2024-04-22 00:22:31 +0200

    further docs update

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   modules: ['@nuxtjs/plausible', '@nuxtlabs/github-module'],
+  experimental: {
+    // Need this otherwise vue-server-renderer not found
+    externalVue: false
+  },
 
   github: {
     owner: 'Baroshem',
