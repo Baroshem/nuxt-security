b7497dfeeefa0360114aa275bca8d1b6a127ac55
Michael Brevard <yonshi29@gmail.com>
2024-04-26 12:47:04 +0300

    fix: use string replace

--- src/runtime/nitro/plugins/80-cspSsrNonce.ts
+++ src/runtime/nitro/plugins/80-cspSsrNonce.ts
@@ -1,6 +1,9 @@
 import { defineNitroPlugin } from '#imports'
 import { resolveSecurityRules } from '../utils'
 
+const LINK_RE = /<link([^>]*?>)/g
+const SCRIPT_RE = /<script([^>]*?>)/g
+const STYLE_RE = /<style([^>]*?>)/g
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:html', (html, { event }) => {
@@ -22,13 +25,20 @@ export default defineNitroPlugin((nitroApp) => {
     const sections = ['body', 'bodyAppend', 'bodyPrepend', 'head'] as Section[]
     const cheerios = event.context.security.cheerios!
     for (const section of sections) {
-      cheerios[section].forEach($ => {
+      cheerios[section] = cheerios[section].map($ => {
         // Add nonce to all link tags
-        $('link').attr('nonce', nonce)
+        $ = $.replace(LINK_RE, (match, rest)=>{
+          return `<link nonce="${nonce}"` + rest
+        })
         // Add nonce to all script tags
-        $('script').attr('nonce', nonce)
+        $ = $.replace(SCRIPT_RE, (match, rest)=>{
+          return `<script nonce="${nonce}"` + rest
+        })
         // Add nonce to all style tags
-        $('style').attr('nonce', nonce)
+        $ = $.replace(STYLE_RE, (match, rest)=>{
+          return `<style nonce="${nonce}"` + rest
+        })
+        return $
       })
     }
   })
