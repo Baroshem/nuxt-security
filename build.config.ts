34c2fc01b13dd8a3e1608b2594cbf7d9183ed8a9
Sébastien Raffray <sraffray@yahoo.fr>
2024-05-07 19:51:14 +0200

    move unbuild config to package.json

--- build.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineBuildConfig } from "unbuild"
-export default defineBuildConfig({
-  entries: [
-    'src/utils/hash.ts',
-    'src/utils/headers.ts',
-    'src/utils/merge.ts'
-  ]
-})
