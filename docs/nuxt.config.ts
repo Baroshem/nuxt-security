fbc39a703c2a359a06e8751951263327e7c0a22a
Sébastien Raffray <sraffray@yahoo.fr>
2024-05-11 12:18:30 +0200

    add security to docs

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
-  modules: ['@nuxtjs/plausible', '@nuxtlabs/github-module'],
+  modules: ['@nuxtjs/plausible', '@nuxtlabs/github-module', 'nuxt-security'],
   experimental: {
     // Need this otherwise vue-server-renderer not found
     externalVue: false
@@ -10,5 +10,14 @@ export default defineNuxtConfig({
     owner: 'Baroshem',
     repo: 'nuxt-security',
     branch: 'main'
+  },
+
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        'img-src': ["'self'", "data:", 'https:'] // Allow https: external images
+      },
+      crossOriginEmbedderPolicy: 'credentialess' // Allow youtube and stackblitz iframes
+    }
   }
 })
