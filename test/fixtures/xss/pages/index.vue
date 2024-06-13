aef9671461d128257294bab58be6deae8cc99561
Michael Brevard <yonshi29@gmail.com>
2024-06-13 14:01:48 +0300

    Update index.vue

--- test/fixtures/xss/pages/index.vue
+++ test/fixtures/xss/pages/index.vue
@@ -1,3 +1,22 @@
 <template>
   <div>basic</div>
+  <div>
+    <button @click="onClick">
+      Click to emit event
+    </button>
+  </div>
 </template>
+
+<script lang="ts" setup>
+const io = useIO()
+
+const onClick = () => {
+  const socket = io()
+
+  socket.on('connect', () => {
+    socket.emit('id:req', (res) => {
+      console.log(res)
+    })
+  })
+}
+</script>
