faaeb69d356e050a76d9dfcf8522a7c81b952318
Sébastien Raffray <sraffray@yahoo.fr>
2024-11-19 14:18:45 +0100

    do not remove loggers in dev mode

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -5,5 +5,6 @@
 </template>
 
 <script setup lang="ts">
+console.log('hello from index should be removed in production')
 const { data } = await useAsyncData(async () => await Promise.resolve("Home"));
 </script>
